a88ef9c2c79a9da89baa185192e5b6cd8e8e9a1d
bogdan-klimov <bodyaklimov333@gmail.com>
2021-04-09 15:21:07 +0300

    firstCommit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <img src={logo} className="AppLogo" alt="logo" />
+        <span className="AppText">
+            React!
+        </span>
+      </div>
+    </div>
+  );
+}
+
+
+export default App;
